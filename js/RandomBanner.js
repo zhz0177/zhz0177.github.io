f098a5b2cc74ceaa8c54def328fb654d9db4746f
zhz0177 <952272858@qq.com>
2025-11-29 14:43:51 +0800

    Site updated: 2025-11-29 14:43:50

--- js/RandomBanner.js
+++ js/RandomBanner.js
@@ -1,18 +1,18 @@
 const imgs = [
     // paths to your banner images
-    "/img/banner/1.png",
-    "/img/banner/2.png",
-    "/img/banner/3.png",
-    "/img/banner/4.png",
-    "/img/banner/5.png",
-    "/img/banner/6.png",
-    "/img/banner/7.png",
-    "/img/banner/8.png",
-    "/img/banner/9.png",
-    "/img/banner/10.png",
-    "/img/banner/11.png",
-    "/img/banner/12.png",
-    "/img/banner/13.png",
+    "/img/banner/1.webp",
+    "/img/banner/2.webp",
+    "/img/banner/3.webp",
+    "/img/banner/4.webp",
+    "/img/banner/5.webp",
+    "/img/banner/6.webp",
+    "/img/banner/7.webp",
+    "/img/banner/8.webp",
+    "/img/banner/9.webp",
+    "/img/banner/10.webp",
+    "/img/banner/11.webp",
+    "/img/banner/12.webp",
+    "/img/banner/13.webp",
 ]
 
 const random_banner = imgs[Math.floor(Math.random() * imgs.length)];
