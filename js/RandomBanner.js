f4c1feadb16fdfbb43c07a087907302f4f6007c9
zhz0177 <952272858@qq.com>
2025-11-29 15:11:16 +0800

    Site updated: 2025-11-29 15:11:16

--- js/RandomBanner.js
+++ js/RandomBanner.js
@@ -1,5 +1,4 @@
 const imgs = [
-    // paths to your banner images
     "/img/banner/1.webp",
     "/img/banner/2.webp",
     "/img/banner/3.webp",
@@ -13,17 +12,15 @@ const imgs = [
     "/img/banner/11.webp",
     "/img/banner/12.webp",
     "/img/banner/13.webp",
-]
+];
 
-const random_banner = imgs[Math.floor(Math.random() * imgs.length)];
 const banner = document.getElementById('banner');
 if (banner) {
     const metaOgType = document.querySelector('meta[property="og:type"]');
-    console.log(" metaOgType.content: ", metaOgType ? metaOgType.content : "not found");
-    if (metaOgType && metaOgType.content === "article") { //判断是否为文章页
-        const background = banner.style.background;
-        if (background.includes("/img/banner/random.webp")) { // 特殊判断规则
-            banner.style.background = `url(${random_banner}) center center / cover no-repeat`;
-        }
+    // 判断是否为文章页
+    if (metaOgType && metaOgType.content === "article") {
+        const random_banner = imgs[Math.floor(Math.random() * imgs.length)];
+        banner.style.background = `url(${random_banner}) center center / cover no-repeat`;
+        console.log("随机文章背景已设置：", random_banner);
     }
 }
